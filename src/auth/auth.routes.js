f60862c5b19d8fcdec9858adcc7279e14a400348
CarloErnestoPriego <cpriego-2020544@kinal.edu.gt>
2025-02-21 16:53:51 -0600

    carpeta auths terminada

---
+++ src/auth/auth.routes.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import {login, registrarUsuario} from "./auth.controller.js"
+import { deleteFileOnError } from "../middlewares/delete-file-on-errors.js";
+import { loginValidator, registrarUsuarioValidator } from "../middlewares/validadores.js";
+
+const router = Router()
+
+//Registrar usuarios
+//URL DEL ENDPOINT: http://localhost:3000/sistemaGestionAcademica/v1/auth/registrarUsuario
+router.post(
+    "/registrarUsuario",
+    registrarUsuarioValidator,
+    deleteFileOnError,
+    registrarUsuario
+)
+//Login
+//URL DEL ENDPOINT: http://localhost:3000/sistemaGestionAcademica/v1/auth/login
+router.post(
+    "/login",
+    loginValidator,
+    deleteFileOnError,
+    login
+)
+
+export default router
